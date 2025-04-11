d2ad0df26e13e9f8e37f6af50c002600f6ab5e40
codayon <reshadahammedayon@proton.me>
2025-04-11 23:55:49 +0600

    feat: practice basic JS variables and console output

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 // printing hello world
 var greetings = 'hello world!';
+greetings = 'hello dlorw!';
 console.log(greetings);
+
+// printing hello world in
+let salvete = 'salve verbum!';
+salvete = 'salve mubrev!';
+console.log(salvete);
+
+// printing hello world in spanish
+const saludos = 'hola palabra!';
+console.log(saludos);
+
+// printing in lines
+const name = 'Reshad Ahammed Ayon';
+const age = 19;
+const food = true;
+console.log('My name is' + ' ' + name + '.');
+console.log('I am' + ' ' + age + ' ' + 'years old.');
+console.log('Biriyani is delicious:' + ' ' + food + '.');
