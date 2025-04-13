1eded6175aed51c3fa05c65bff1be1307ee6ddef
codayon <reshadahammedayon@proton.me>
2025-04-13 20:19:28 +0600

    done with homework #28

--- main.js
+++ main.js
@@ -1,21 +1,55 @@
-// printing hello world
-var greetings = 'hello world!';
-greetings = 'hello dlorw!';
-console.log(greetings);
+// ==============================
+// printing hello world with var
+// ==============================
+// var greetings = 'hello world!';
+// greetings = 'hello dlorw!';
+// console.log(greetings);
 
-// printing hello world in
-let salvete = 'salve verbum!';
-salvete = 'salve mubrev!';
-console.log(salvete);
+// =================================
+// printing hello world in with let
+// =================================
+// let salvete = 'salve verbum!';
+// salvete = 'salve mubrev!';
+// console.log(salvete);
 
-// printing hello world in spanish
-const saludos = 'hola palabra!';
-console.log(saludos);
+// ===========================================
+// printing hello world in spanish with const
+// ===========================================
+// const saludos = 'hola palabra!';
+// console.log(saludos);
 
-// printing in lines
-const name = 'Reshad Ahammed Ayon';
-const age = 19;
-const food = true;
-console.log('My name is' + ' ' + name + '.');
-console.log('I am' + ' ' + age + ' ' + 'years old.');
-console.log('Biriyani is delicious:' + ' ' + food + '.');
+// ======================
+// printing inside lines
+// ======================
+// const namez = 'Reshad Ahammed Ayon';
+// const agez = 19;
+// const foodz = true;
+// console.log('My name is' + ' ' + namez + '.');
+// console.log('I am' + ' ' + agez + ' ' + 'years old.');
+// console.log('Biriyani is delicious:' + ' ' + foodz + '.');
+
+// ====================================
+// using scope with var, let and const
+// ====================================
+// {
+//   var namez = 'Reshad Ahammed Ayon';
+// }
+// {
+//   console.log('My name is' + ' ' + namez + '.');
+// }
+// {
+//   let agez = 19;
+//   const foodz = true;
+//   console.log('I am' + ' ' + agez + ' ' + 'years old.');
+//   console.log('Biriyani is delicious:' + ' ' + foodz + '.');
+// }
+
+// ============================
+// checking the type of syntax
+// ============================
+const namez = 'Reshad Ahammed Ayon';
+const agez = 19;
+const foodz = true;
+console.log(typeof namez);
+console.log(typeof agez);
+console.log(typeof foodz);
