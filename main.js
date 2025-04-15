19c0e9d27cd9734801766fda42855b0f64b2549b
codayon <reshadahammedayon@proton.me>
2025-04-16 00:23:39 +0600

    homework: complete

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
-// ==============================
-// printing hello world with var
-// ==============================
-// var greetings = 'hello world!';
-// greetings = 'hello dlorw!';
-// console.log(greetings);
+// dumb project with js (learning)
 
-// =================================
-// printing hello world in with let
-// =================================
-// let salvete = 'salve verbum!';
-// salvete = 'salve mubrev!';
-// console.log(salvete);
+// using if and else
+let gender = 'male';
+let rich = true;
+let age = 21;
 
-// ===========================================
-// printing hello world in spanish with const
-// ===========================================
-// const saludos = 'hola palabra!';
-// console.log(saludos);
+if (gender == 'male') {
+  console.log('you are a male');
+} else {
+  console.log('you are a female');
+}
 
-// ======================
-// printing inside lines
-// ======================
-// const namez = 'Reshad Ahammed Ayon';
-// const agez = 19;
-// const foodz = true;
-// console.log('My name is' + ' ' + namez + '.');
-// console.log('I am' + ' ' + agez + ' ' + 'years old.');
-// console.log('Biriyani is delicious:' + ' ' + foodz + '.');
+if (rich == true) {
+  console.log('you are rich');
+} else {
+  console.log('you are poor');
+}
 
-// ====================================
-// using scope with var, let and const
-// ====================================
-// {
-//   var namez = 'Reshad Ahammed Ayon';
-// }
-// {
-//   console.log('My name is' + ' ' + namez + '.');
-// }
-// {
-//   let agez = 19;
-//   const foodz = true;
-//   console.log('I am' + ' ' + agez + ' ' + 'years old.');
-//   console.log('Biriyani is delicious:' + ' ' + foodz + '.');
-// }
+if (age == 21) {
+  console.log('you are eligible to get married');
+} else {
+  console.log('you are kid');
+}
 
-// ============================
-// checking the type of syntax
-// ============================
-const namez = 'Reshad Ahammed Ayon';
-const agez = 19;
-const foodz = true;
-console.log(typeof namez);
-console.log(typeof agez);
-console.log(typeof foodz);
+console.log('================================');
+
+// using nested if
+let student = true;
+let classroom = 'mern';
+let sid = 2354;
+
+if (student == true) {
+  if (classroom == 'mern') {
+    if (sid == 2354) {
+      console.log('welcome to your classroom');
+    }
+  }
+} else {
+  console.log('you are not allowed here');
+}
+
+console.log('==========================');
+
+// using else if
+let score = 0;
+
+if (score > 100 || score < 0) {
+  console.log('your score cannot be more than 100 or less than 0');
+} else if (score >= 80) {
+  console.log('your grade is a+');
+} else if (score >= 70) {
+  console.log('your grade is a');
+} else if (score >= 60) {
+  console.log('your grade is a-');
+} else if (score >= 50) {
+  console.log('your grade is b');
+} else if (score >= 40) {
+  console.log('your grade is c');
+} else if (score >= 33) {
+  console.log('your grade is d');
+} else {
+  console.log('you have failed to pass');
+}
