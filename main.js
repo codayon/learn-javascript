41fe1ea2e22d1d133be5a320069c4f648f5358ce
codayon <reshadahammedayon@proton.me>
2025-04-17 23:44:36 +0600

    learn: arrays, objects, for loop

--- main.js
+++ main.js
@@ -1,64 +1,36 @@
 // dumb project with js (learning)
 
-// using if and else
-let gender = 'male';
-let rich = true;
-let age = 21;
+// arrays
 
-if (gender == 'male') {
-  console.log('you are a male');
-} else {
-  console.log('you are a female');
-}
+let x = ['x', 'y', 'z'];
 
-if (rich == true) {
-  console.log('you are rich');
-} else {
-  console.log('you are poor');
-}
+console.log(x[0]);
 
-if (age == 21) {
-  console.log('you are eligible to get married');
-} else {
-  console.log('you are kid');
-}
+// objects
 
-console.log('================================');
+let y = {
+  x: 'x',
+  y: 'y',
+  z: 'z',
+};
 
-// using nested if
-let student = true;
-let classroom = 'mern';
-let sid = 2354;
+console.log(y.y);
 
-if (student == true) {
-  if (classroom == 'mern') {
-    if (sid == 2354) {
-      console.log('welcome to your classroom');
-    }
-  }
-} else {
-  console.log('you are not allowed here');
-}
+// defined objects
+let z = {
+  x: 'x',
+  y: 'y',
+  z: 'z',
+};
 
-console.log('==========================');
+console.log(z.z);
 
-// using else if
-let score = 0;
+console.log('==========');
 
-if (score > 100 || score < 0) {
-  console.log('your score cannot be more than 100 or less than 0');
-} else if (score >= 80) {
-  console.log('your grade is a+');
-} else if (score >= 70) {
-  console.log('your grade is a');
-} else if (score >= 60) {
-  console.log('your grade is a-');
-} else if (score >= 50) {
-  console.log('your grade is b');
-} else if (score >= 40) {
-  console.log('your grade is c');
-} else if (score >= 33) {
-  console.log('your grade is d');
-} else {
-  console.log('you have failed to pass');
+// for loop
+
+let number = 7;
+
+for (i = 1; i <= 10; ++i) {
+  console.log(number + ' x ' + i + ' = ' + number * i);
 }
