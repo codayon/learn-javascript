e560b3a6400906d8ced1dfbd5be4e307f284efa6
codayon <reshadahammedayon@proton.me>
2025-04-23 19:39:03 +0600

    task: homework completed!

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-// dumb project with js (learning)
+  ///////////////////////////
+ /// learning javascript ///
+///////////////////////////
 
-// arrays
 
-let x = ['x', 'y', 'z'];
 
-console.log(x[0]);
+// use of backtick
+let title = "fighter";
+console.log(`conor is a ${title}`);
 
-// objects
+// use of function
+function math (){
+	let one = 1;
+	let two = 2;
+	console.log (`one and two equals to ${one + two}`);
+};
+math ();
 
-let y = {
-  x: 'x',
-  y: 'y',
-  z: 'z',
+// project one
+let members = ['don', 'john', 'ron']
+for (let i = 0; i < members.length; i++){
+	console.log (`let's welcome our new memeber ${members[i]}!`);
 };
 
-console.log(y.y);
-
-// defined objects
-let z = {
-  x: 'x',
-  y: 'y',
-  z: 'z',
-};
-
-console.log(z.z);
-
-console.log('==========');
-
-// for loop
-
-let number = 7;
-
-for (i = 1; i <= 10; ++i) {
-  console.log(number + ' x ' + i + ' = ' + number * i);
-}
+// project two: using map 
+let students = ['joe', 'bob', 'ben']
+students.map (useless=>{
+	console.log (`and, let's not welcome ${useless}.`)
+});
