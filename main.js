16b1e408f8a83e7f306e8d6272dd81aa0b0fd347
codayon <reshadahammedayon@proton.me>
2025-05-17 23:52:06 +0600

    practice: js output from w3schools

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 console.log("learning javascript");
 console.log("<=================>");
+
+/**
+ * Source: W3Schools
+ * Topic: JS Output
+ * Property: innerHTML
+ * Description: Use when you want to change an HTML element.
+ */
+
+document.getElementById("innerHTML").innerHTML = "<h2>innerHTML</h2>";
+
+/**
+ * Property: innerText
+ * Description: Use when you only want to change the plain text.
+ */
+
+document.getElementById("innerText").innerText = "innerText";
+
+/**
+ * Method: write()
+ * Description: For testing purposes, it is convenient to use it
+ * Note: The document.write() method should only be used for testing.
+ */
+
+document.write("write() before");
+
+/**
+ * Method: window.alert()
+ * Description: Use an alert box to display data.
+ * Note: In JavaScript, the window object is the global scope object. This means that variables, properties, and methods by default belong to the window object. This also means that specifying the window keyword is optional.
+ */
+
+window.alert("window.alert()");
